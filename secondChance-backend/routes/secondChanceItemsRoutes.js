2fb13d4a0198d105d1555602603205a7b09273e4
Pefam <159665558+Pefam@users.noreply.github.com>
2025-05-03 22:13:40 -0600

    Update secondChanceItemsRoutes.js

--- secondChance-backend/routes/secondChanceItemsRoutes.js
+++ secondChance-backend/routes/secondChanceItemsRoutes.js
@@ -118,23 +118,23 @@ router.put('/:id', async (req, res, next) => {
 })
 
 // Delete an existing item
-router.delete('/:id', async(req, res,next) => {
+router.delete('/:id', async (req, res, next) => {
   try {
-    const id = req.params.id;
+    const id = req.params.id
     // Step 6: task 1 - insert code here
     const db = await connectToDatabase()
     // Step 6: task 2 - insert code here
-    const collection = db.collection("secondChanceItems")
+    const collection = db.collection('secondChanceItems')
     // Step 6: task 3 - insert code here
     const secondChanceItem = await collection.findOne({ id })
 
     if (!secondChanceItem) {
       logger.error('secondChanceItem not found')
-      return res.status(404).json({ error: "secondChanceItem not found" })
+      return res.status(404).json({ error: 'secondChanceItem not found' })
     }
     // Step 6: task 4 - insert code here
     await collection.deleteOne({ id })
-      res.json({"deleted":"success"})
+    res.json({ deleted: 'success' })
     } catch (e) {
       next(e)
     }

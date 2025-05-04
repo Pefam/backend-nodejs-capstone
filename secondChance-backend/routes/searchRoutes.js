0f9d3cf7ae53696b2aca67aa136834a9f9c30e16
Pefam <159665558+Pefam@users.noreply.github.com>
2025-05-03 22:22:52 -0600

    Update searchRoutes.js

--- secondChance-backend/routes/searchRoutes.js
+++ secondChance-backend/routes/searchRoutes.js
@@ -11,11 +11,11 @@ router.get('/', async (req, res, next) => {
     const collection = db.collection('secondChanceItems')
 
     // Initialize the query object
-    let query = {}
+    const query = {}
 
     // Add the name filter to the query if the name parameter is not empty
     if (req.query.name && req.query.name.trim() !== '') {
-      query.name = { $regex: req.query.name, $options: "i" } // Using regex for partial match, case-insensitive
+      query.name = { $regex: req.query.name, $options: 'i' } // Using regex for partial match, case-insensitive
     }
 
     // Task 3: Add other filters to the query
@@ -23,7 +23,7 @@ router.get('/', async (req, res, next) => {
       query.category = req.query.category
     }
     if (req.query.condition) {
-      query.condition = req.query.condition 
+      query.condition = req.query.condition
     }
     if (req.query.age_years) {
       query.age_years = { $lte: parseInt(req.query.age_years) }
